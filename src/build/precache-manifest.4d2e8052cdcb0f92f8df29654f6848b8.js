cfdcd952297fc875657c6000928a51e24d4b097c
danny_dev <dfeluduxyz@yahoo.co.uk>
2020-01-27 17:50:57 +0100

    First Commit

---
+++ src/build/precache-manifest.4d2e8052cdcb0f92f8df29654f6848b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465bab19fdd435e7877afba155328298",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ab66b5dba7d483d3310",
+    "url": "/static/css/main.84b4ca96.chunk.css"
+  },
+  {
+    "revision": "e3456c03bbaf969b866b",
+    "url": "/static/js/2.4f3e10bd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.4f3e10bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ab66b5dba7d483d3310",
+    "url": "/static/js/main.5f2c8743.chunk.js"
+  },
+  {
+    "revision": "2829fc8889d8b91e8c4b",
+    "url": "/static/js/runtime-main.b519a9b9.js"
+  }
+]);
